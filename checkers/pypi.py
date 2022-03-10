d546274bdd7821f73b50b391a8df82c923ecac49
ViZus <86660567+ViZus-s@users.noreply.github.com>
2022-03-10 17:44:46 +0300

    /checkers/pypi.py

---
+++ checkers/pypi.py
@@ -0,0 +1,8 @@
+"""
+This file contains various functions for checking arguments when parsing pypi.
+This is necessary for parsing to work normally with using api;
+otherwise, without it, they will give errors related to the date.
+"""
+"""
+Code in the process!!
+"""
