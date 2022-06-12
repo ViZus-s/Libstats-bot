d933e5b70d0cf0b58b027a7e83a6a5fb7368f286
ViZus <86660567+ViZus-s@users.noreply.github.com>
2022-06-12 09:32:07 +0300

    Timestamp update

--- parsers/gitthub.py
+++ parsers/gitthub.py
@@ -1,8 +1,9 @@
 # libs
 from bs4 import BeautifulSoup
 import requests as req
-# parse
+import time
 
+# parse
 def parsing_git():
     response = req.get("https://github.com/DisnakeDev/disnake/commit/master")
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -11,18 +12,20 @@ def parsing_git():
     forks = soup.find("span", {'class': "Counter"}).text
     issues = soup.find("span", {"id": "issues-repo-tab-count"}).text
     pull_requests = soup.find("a", {"id": "pull-requests-tab"}).text
+
     last_commit = str(soup.find("relative-time")).split(
-        ">", maxsplit=1)[0].split("datetime=")[-1][1:-2].replace("T", " ")
+    ">", maxsplit=1)[0].split("datetime=")[-1][1:-2].replace("T", " ")
+    
+    last_commit = f"<t:{int(time.mktime(time.strptime(last_commit, '%Y-%m-%d %H:%M:%S')))}>"
 
     return {"stars": stars,
-                  "forks": forks,
-                  "issues": issues,
-                  "pull requests": pull_requests.replace("\n", "")[13:],
-                  "last commit": last_commit}
+            "forks": forks,
+            "issues": issues,
+            "pull requests": pull_requests.replace("\n", "")[13:],
+            "last commit": last_commit,}
 
 
 def parsing_update():
-    response = req.get(
-        "https://github.com/ViZus-s/Disnake-Statistics-Bot/commit/")
+    response = req.get("https://github.com/ViZus-s/Disnake-Statistics-Bot/commit/")
     soup = BeautifulSoup(response.content, 'html.parser')
     return soup.find('div', {"class": "commit-title markdown-title"}).text.strip()
