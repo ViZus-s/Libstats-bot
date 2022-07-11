319be61a6fa7f0b3dd9bd3be0be69f884a0216b8
ViZus <86660567+ViZus-s@users.noreply.github.com>
2022-07-11 11:19:14 +0300

    date bug fix + PEP8.

--- parsers/pypi.py
+++ parsers/pypi.py
@@ -1,33 +1,29 @@
 # libs
 from bs4 import BeautifulSoup
-from datetime import date
+from datetime import date, timedelta
 import grequests as req
 import json
 import time
 
 # links
 
-links = ["https://pypistats.org/packages/disnake", 
-"https://api.pepy.tech/api/v2/projects/disnake",]
-
+links = ["https://pypistats.org/packages/disnake",
+         "https://api.pepy.tech/api/v2/projects/disnake"]
 # parse
+
 
 def parsing_pypi():
     link_get = [req.get(link) for link in links]
     responses = req.map(link_get)
-    
+
     soup1 = BeautifulSoup(responses[0].content, 'html.parser')
     reqs = responses[1].text
     reqs = json.loads(reqs)
 
     div1 = soup1.find("div", {"class": "wrapper"}).text.split()
 
-    try:
-        last_day = str(date.today())
-        downloads_list = list(reqs["downloads"][last_day])
-    except KeyError:
-        last_day = str(date.today())[:-1] + str(int(str(date.today())[-1]) - 1)
-        downloads_list = reqs["downloads"][last_day]
+    last_day = str(date.today() - timedelta(days=1))
+    downloads_list = reqs["downloads"][last_day]
     items_list = list(downloads_list.values())
 
     return {
@@ -39,4 +35,4 @@ def parsing_pypi():
         "downloads_sum": "{:,}".format(sum(items_list)),
         "last_version_downloads": "{:,}".format(downloads_list[reqs['versions'][-1]]),
         "set": last_day,
-    },
+    }
