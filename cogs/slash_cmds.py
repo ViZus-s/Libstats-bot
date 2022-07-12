7e0014f76067cccb7b415ad20beb995a7673f5ba
ViZus <86660567+ViZus-s@users.noreply.github.com>
2022-07-12 17:59:24 +0300

    nextcord support

--- cogs/slash_cmds.py
+++ cogs/slash_cmds.py
@@ -16,10 +16,11 @@ class Slashs(commands.Cog):
         name="stats",
         description="Wanna check statistic? Use this command!",
     )
-    async def stats(self, inter: ApplicationCommandInteraction, option: str = commands.Param(choices=['pypi', 'github'])):
+    async def stats(self, inter: ApplicationCommandInteraction, platform: str = commands.Param(choices=['pypi', 'github']), library: str = commands.Param(choices=['disnake', 'nextcord'])):
         member = inter.author
-        if option == 'github':
-            get_data = parsing_git()
+
+        if platform == 'github':
+            get_data = parsing_git(library)
 
             stars = get_data["stars"]
             forks = get_data["forks"]
@@ -27,16 +28,16 @@ class Slashs(commands.Cog):
             pull = get_data["pull requests"]
             lastcom = get_data["last commit"]
 
-            embed1 = disnake.Embed(title="GitHub Disnake Statistics",
+            embed1 = disnake.Embed(title=f"GitHub {library.capitalize()} Statistics",
                                    description=f":star: **stars**: `{stars}`\n:cd: **forks**: `{forks}`\n:bangbang: **issues**: `{issues}`\n:satellite: **pull requests**: `{pull}`\n:hourglass: **last commit**: {lastcom}",
                                    color=glcolor)
 
             embed1.set_author(name=member, icon_url=member.avatar.url)
             await inter.send(embed=embed1)
 
-        elif option == 'pypi':
+        elif platform == 'pypi':
             try:
-                get_data = parsing_pypi()
+                get_data = parsing_pypi(library)
 
                 last_version1 = get_data[0]['last_version']
                 downloads1 = get_data[0]['downloads']
@@ -45,15 +46,15 @@ class Slashs(commands.Cog):
                 total_downloads2 = get_data[1]['total_downloads']
                 set2 = get_data[1]['set']
                 downloads_sum2 = get_data[1]['downloads_sum']
-
+                
                 embed2 = disnake.Embed(
-                    title="PyPi Disnake Statistics",
+                    title=f"PyPi {library.capitalize()} Statistics",
                     description=f":satellite: `pypistats`\n:bulb: **Latest Version**: {last_version1}\n:pencil: **Downloads last day:** {downloads1[0]}\n:pencil: **Downloads last week:** {downloads1[1]}\n:pencil: **Downloads last month:** {downloads1[2]}\n\n:satellite: `api-pepy-tech`\n:bulb: **Latest Version**: {last_version2}\n:bar_chart: **Last version downloads**: {last_version_downloads2}\n:chart_with_upwards_trend: **Total downloads**: {total_downloads2}\n:calendar_spiral: **Downloads on {set2}**: {downloads_sum2}\n",
                     color=glcolor)
                 embed2.set_author(name=member, icon_url=member.avatar.url)
                 await inter.send(embed=embed2)
             except:
-                await inter.send("The pypi command is not working right now, forgive us for that, the problem was identified in the api.")
+                await inter.send("The pypi command is not working right now, forgive us for that, the problem was identified in the api.", error)
 
     @commands.slash_command(
         name="help",
@@ -75,7 +76,7 @@ class Slashs(commands.Cog):
 
         embed = disnake.Embed(
             title="Bot information",
-            description=f"<:disnake:922937443039186975> **Disnake version**: `{disnake.__version__}`\n<:icon2:983249100558434355>  **Python version**: `{version[:6:]}`\n:timer: **Ping**: `{round(self.bot.latency * 1000)} ms`\n:envelope_with_arrow: **Last update**: `{parsing_update()}`\n:open_file_folder: **Bot github**: ||https://github.com/ViZus-s/Disnake-Statistics-Bot||",
+            description=f"<:disnake:922937443039186975> **Disnake version**: `{disnake.__version__}`\n<:icon2:983249100558434355> **Python version**: `{version[:6:]}`\n:timer: **Ping**: `{round(self.bot.latency * 1000)} ms`\n:envelope_with_arrow: **Last update**: `{parsing_update()}`\n:gear: **Lib support**: `nextcord`, `disnake`\n:open_file_folder: **Bot github**: ||https://github.com/ViZus-s/Libstats-bot||",
             color=glcolor)
 
         embed.set_author(name=inter.author, icon_url=inter.author.avatar.url)
