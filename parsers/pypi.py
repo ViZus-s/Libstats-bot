735aee7f945205386c30520807fe49211ece024a
ViZus <86660567+ViZus-s@users.noreply.github.com>
2022-07-24 12:06:56 +0300

    discord.py pypi support

--- parsers/pypi.py
+++ parsers/pypi.py
@@ -9,14 +9,22 @@ import time
 
 links = {"disnake": ["https://pypistats.org/packages/disnake",
                      "https://api.pepy.tech/api/v2/projects/disnake"],
+
          "nextcord": ["https://pypistats.org/packages/nextcord",
                       "https://api.pepy.tech/api/v2/projects/nextcord"],
+
          "pycord": ["https://pypistats.org/packages/py-cord",
-                    "https://api.pepy.tech/api/v2/projects/py-cord"]}
+                    "https://api.pepy.tech/api/v2/projects/py-cord"],
+
+         "discord.py": ["https://pypistats.org/packages/discord.py",
+                        "https://api.pepy.tech/api/v2/projects/discord.py"],
+         }
+
 # parse
 
 
 def parsing_pypi(library: str):
+
     link_get = [req.get(link) for link in links[library]]
     responses = req.map(link_get)
 
@@ -32,15 +40,21 @@ def parsing_pypi(library: str):
 
     if library == "disnake":
 
+        last_version2 = reqs['versions'][-1]
+
         return {
+
             "last_version": div1[29],
             "downloads": div1[57::4],
+
         }, {
-            "last_version": reqs['versions'][-1],
+
+            "last_version": last_version2,
             "total_downloads": "{:,}".format(reqs['total_downloads']),
             "downloads_sum": "{:,}".format(sum(items_list)),
-            "last_version_downloads": "{:,}".format(downloads_list[reqs['versions'][-1]]),
+            "last_version_downloads": "{:,}".format(downloads_list[last_version2]),
             "set": last_day,
+
         }
 
     elif library == "nextcord":
@@ -51,14 +65,18 @@ def parsing_pypi(library: str):
         last_version2 = ".".join(str(sorted(last_version2)[-1]))
 
         return {
+
             "last_version": div1[33],
             "downloads": div1[37::4],
+
         }, {
+
             "last_version": last_version2,
             "total_downloads": "{:,}".format(reqs['total_downloads']),
             "downloads_sum": "{:,}".format(sum(items_list)),
             "last_version_downloads": "{:,}".format(downloads_list[last_version2]),
             "set": last_day
+
         }
 
     elif library == "pycord":
@@ -69,12 +87,35 @@ def parsing_pypi(library: str):
         last_version2 = ".".join(str(sorted(last_version2)[-1]))
 
         return {
+
             "last_version": div1[29],
             "downloads": div1[51::4][1:],
+
         }, {
+
             "last_version": last_version2,
             "total_downloads": "{:,}".format(reqs['total_downloads']),
             "downloads_sum": "{:,}".format(sum(items_list)),
             "last_version_downloads": "{:,}".format(downloads_list[last_version2]),
             "set": last_day
+
+        }
+
+    elif library == "discord.py":
+
+        last_version2 = reqs['versions'][-1]
+
+        return {
+
+            "last_version": div1[28],
+            "downloads": div1[40::4][1:]
+
+        }, {
+
+            "last_version": last_version2,
+            "total_downloads": "{:,}".format(reqs['total_downloads']),
+            "downloads_sum": "{:,}".format(sum(items_list)),
+            "last_version_downloads": "{:,}".format(downloads_list[last_version2]),
+            "set": last_day
+
         }
