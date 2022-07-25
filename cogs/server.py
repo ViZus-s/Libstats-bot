b3a45de04229b25e67e2d9ab199b73faf513684c
ViZus <86660567+ViZus-s@users.noreply.github.com>
2022-07-25 21:45:38 +0300

    Class name fix

--- cogs/server.py
+++ cogs/server.py
@@ -1,10 +1,9 @@
-# imports
 import disnake
 from disnake.ext import commands
 
-class server(commands.Cog):
+class Server(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
 def setup(bot):
-    bot.add_cog(server(bot))
+    bot.add_cog(Server(bot))
