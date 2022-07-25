64ecdd2dac677545b451f3d70dffe08a84983c59
ViZus <86660567+ViZus-s@users.noreply.github.com>
2022-07-25 21:40:56 +0300

    interactions.py pypi support

--- parsers/pypi.py
+++ parsers/pypi.py
@@ -1,31 +1,30 @@
-# libs
-from bs4 import BeautifulSoup
-from datetime import date, timedelta
-import grequests as req
-import json
 import time
+import json
+from datetime import date, timedelta
 
-# links
+import grequests as req
+from bs4 import BeautifulSoup
 
-links = {"disnake": ["https://pypistats.org/packages/disnake",
-                     "https://api.pepy.tech/api/v2/projects/disnake"],
+LINKS = {
+    "disnake": ("https://pypistats.org/packages/disnake",
+                "https://api.pepy.tech/api/v2/projects/disnake"),
 
-         "nextcord": ["https://pypistats.org/packages/nextcord",
-                      "https://api.pepy.tech/api/v2/projects/nextcord"],
+    "nextcord": ("https://pypistats.org/packages/nextcord",
+                 "https://api.pepy.tech/api/v2/projects/nextcord"),
 
-         "pycord": ["https://pypistats.org/packages/py-cord",
-                    "https://api.pepy.tech/api/v2/projects/py-cord"],
+    "pycord": ("https://pypistats.org/packages/py-cord",
+               "https://api.pepy.tech/api/v2/projects/py-cord"),
 
-         "discord.py": ["https://pypistats.org/packages/discord.py",
-                        "https://api.pepy.tech/api/v2/projects/discord.py"],
-         }
+    "discord.py": ("https://pypistats.org/packages/discord.py",
+                   "https://api.pepy.tech/api/v2/projects/discord.py"),
 
-# parse
-
+    "interactions.py": ("https://pypistats.org/packages/discord-py-interactions",
+                        "https://api.pepy.tech/api/v2/projects/discord-py-interactions"),
+}
 
 def parsing_pypi(library: str):
 
-    link_get = [req.get(link) for link in links[library]]
+    link_get = [req.get(link) for link in LINKS[library]]
     responses = req.map(link_get)
 
     soup1 = BeautifulSoup(responses[0].content, 'html.parser')
@@ -38,9 +37,11 @@ def parsing_pypi(library: str):
     downloads_list = reqs["downloads"][last_day]
     items_list = list(downloads_list.values())
 
-    if library == "disnake":
+    last_version2 = reqs['versions']
+    last_version2.sort(key=lambda x: tuple((x.lstrip("v") + "z").split(".")))
+    last_version2 = last_version2[-1]
 
-        last_version2 = reqs['versions'][-1]
+    if library == "disnake":
 
         return {
 
@@ -59,11 +60,6 @@ def parsing_pypi(library: str):
 
     elif library == "nextcord":
 
-        last_version2 = ([int(i.replace(".", ""))
-                          for i in reqs['versions']
-                          if i.replace(".", "").isdigit()])
-        last_version2 = ".".join(str(sorted(last_version2)[-1]))
-
         return {
 
             "last_version": div1[33],
@@ -80,11 +76,6 @@ def parsing_pypi(library: str):
         }
 
     elif library == "pycord":
-
-        last_version2 = ([int(i.replace(".", ""))
-                          for i in reqs['versions']
-                          if i.replace(".", "").isdigit()])
-        last_version2 = ".".join(str(sorted(last_version2)[-1]))
 
         return {
 
@@ -103,12 +94,27 @@ def parsing_pypi(library: str):
 
     elif library == "discord.py":
 
-        last_version2 = reqs['versions'][-1]
-
         return {
 
             "last_version": div1[28],
             "downloads": div1[40::4][1:]
+
+        }, {
+
+            "last_version": last_version2,
+            "total_downloads": "{:,}".format(reqs['total_downloads']),
+            "downloads_sum": "{:,}".format(sum(items_list)),
+            "last_version_downloads": "{:,}".format(downloads_list[last_version2]),
+            "set": last_day
+
+        }
+
+    elif library == "interactions.py":
+
+        return {
+
+            "last_version": div1[33],
+            "downloads": div1[51::4][1:]
 
         }, {
 
