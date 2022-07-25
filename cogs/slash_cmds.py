a793017dd44726a1256981b8a8097dec233f2b0a
ViZus <86660567+ViZus-s@users.noreply.github.com>
2022-07-25 21:47:21 +0300

    interaction.py in /stats and new /links!

--- cogs/slash_cmds.py
+++ cogs/slash_cmds.py
@@ -1,14 +1,20 @@
-# imports
-import disnake
 from sys import version
+
+import disnake
 from disnake import ApplicationCommandInteraction
 from disnake.ext import commands
-from main import glcolor
-from parsers.gitthub import parsing_git, parsing_update
+
+from main import GLOBAL_COLOR
+from other.links import get_links
+from parsers.github import parsing_git, parsing_update
 from parsers.pypi import parsing_pypi
 
+LIBRARIES = ('disnake', 'nextcord',
+             'pycord', 'discord.py',
+             'interactions.py')
 
-class Slashs(commands.Cog):
+
+class Slashes(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -16,7 +22,7 @@ class Slashs(commands.Cog):
         name="stats",
         description="Wanna check statistic? Use this command!",
     )
-    async def stats(self, inter: ApplicationCommandInteraction, platform: str = commands.Param(choices=['pypi', 'github']), library: str = commands.Param(choices=['disnake', 'nextcord', 'pycord', 'discord.py'])):
+    async def stats(self, inter: disnake.CommandInteraction, platform: str = commands.Param(choices=['pypi', 'github']), library: str = commands.Param(choices=LIBRARIES)):
         member = inter.author
 
         if platform == 'github':
@@ -29,10 +35,11 @@ class Slashs(commands.Cog):
             lastcom = get_data["last commit"]
 
             embed1 = disnake.Embed(title=f"GitHub {library.capitalize()} Statistics",
-                                   description=f":star: **stars**: `{stars}`\n:cd: **forks**: `{forks}`\n:bangbang: **issues**: `{issues}`\n:satellite: **pull requests**: `{pull}`\n:hourglass: **last commit**: {lastcom}",
-                                   color=glcolor)
+                                   description=f":star: **stars**: `{stars}`\n:cd: **forks**: `{forks}`\n:bangbang: **issues**: `{issues}`\n:satellite: **pull requests [OPEN]**: `{pull}`\n:hourglass: **last commit**: {lastcom}",
+                                   color=GLOBAL_COLOR)
 
             embed1.set_author(name=member, icon_url=member.avatar.url)
+            await inter.response.defer()
             await inter.send(embed=embed1)
 
         elif platform == 'pypi':
@@ -50,9 +57,10 @@ class Slashs(commands.Cog):
                 embed2 = disnake.Embed(
                     title=f"PyPi {library.capitalize()} Statistics",
                     description=f":satellite: `pypistats`\n:bulb: **Latest Version**: {last_version1}\n:pencil: **Downloads last day:** {downloads1[0]}\n:pencil: **Downloads last week:** {downloads1[1]}\n:pencil: **Downloads last month:** {downloads1[2]}\n\n:satellite: `api-pepy-tech`\n:bulb: **Latest Version**: {last_version2}\n:bar_chart: **Last version downloads**: {last_version_downloads2}\n:chart_with_upwards_trend: **Total downloads**: {total_downloads2}\n:calendar_spiral: **Downloads on {set2}**: {downloads_sum2}\n",
-                    color=glcolor)
+                    color=GLOBAL_COLOR)
 
                 embed2.set_author(name=member, icon_url=member.avatar.url)
+                await inter.response.defer()
                 await inter.send(embed=embed2)
 
             except:
@@ -63,28 +71,42 @@ class Slashs(commands.Cog):
         name="help",
         description="Shows bot commands",
     )
-    async def help(self, inter: ApplicationCommandInteraction):
+    async def help(self, inter: disnake.CommandInteraction):
 
         embed2 = disnake.Embed(
             title=":book: | Help",
-            description="`/stats` - shows any statistics[github/pypi]\n`/info` - shows bot info",
-            color=glcolor)
+            description="`/stats` - shows any statistics[github/pypi]\n`/info` - shows bot info\n`/links` - shows the links of the selected library",
+            color=GLOBAL_COLOR)
         await inter.send(embed=embed2, ephemeral=True)
 
     @commands.slash_command(
         name="info",
         description="Shows the bot info",
     )
-    async def info(self, inter: ApplicationCommandInteraction):
+    async def info(self, inter: disnake.CommandInteraction):
 
         embed = disnake.Embed(
             title="Bot information",
-            description=f"<:disnake:922937443039186975> **Disnake version**: `{disnake.__version__}`\n<:icon2:983249100558434355> **Python version**: `{version[:6:]}`\n:timer: **Ping**: `{round(self.bot.latency * 1000)} ms`\n:envelope_with_arrow: **Last update**: `{parsing_update()}`\n:gear: **Lib support**: forks, discord.py\n:open_file_folder: **Bot github**: ||https://github.com/ViZus-s/Libstats-bot||",
-            color=glcolor)
+            description=f"<:disnake:922937443039186975> **Disnake version**: `{disnake.__version__}`\n<:icon2:983249100558434355> **Python version**: `{version[:6:]}`\n:timer: **Ping**: `{round(self.bot.latency * 1000)} ms`\n:envelope_with_arrow: **Last update**: `{parsing_update()}`\n:gear: **Lib support**: forks, discord.py, interactions.py\n:open_file_folder: **Bot github**: ||https://github.com/ViZus-s/Libstats-bot||",
+            color=GLOBAL_COLOR)
 
+        embed.set_author(name=inter.author, icon_url=inter.author.avatar.url)
+        await inter.send(embed=embed)
+
+    @commands.slash_command(
+        name="links",
+        description="Shows all selected library links.",
+    )
+    async def links(self, inter: disnake.CommandInteraction, library: str = commands.Param(choices=LIBRARIES)):
+
+        embed = disnake.Embed(
+            title=f"{library.capitalize()} links",
+            description=get_links(library),
+            color=GLOBAL_COLOR
+        )
         embed.set_author(name=inter.author, icon_url=inter.author.avatar.url)
         await inter.send(embed=embed)
 
 
 def setup(bot):
-    bot.add_cog(Slashs(bot))
+    bot.add_cog(Slashes(bot))
