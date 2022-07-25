25f48ace8d97e343863867e65355c27aeadf6648
ViZus <86660567+ViZus-s@users.noreply.github.com>
2022-07-25 21:44:27 +0300

    Class name fix

--- cogs/dev.py
+++ cogs/dev.py
@@ -1,10 +1,9 @@
-# imports
 import disnake
 from disnake.ext import commands
 
-class dev(commands.Cog):
+class Dev(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
 def setup(bot):
-    bot.add_cog(dev(bot))
+    bot.add_cog(Dev(bot))
