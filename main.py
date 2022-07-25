9fa4c917d76852d02f6793d8a9c0d8072b3b4592
ViZus <86660567+ViZus-s@users.noreply.github.com>
2022-07-25 21:36:58 +0300

    mini-update

--- main.py
+++ main.py
@@ -1,38 +1,27 @@
-# -------------------------- monkey
+import os
+
 from gevent import monkey as curious_george
 curious_george.patch_all(thread=False, select=False)
-# -------------------------- imports
-import os
-import disnake
+
+# patch first
+
 from disnake.ext import commands
-# -------------------------- global color
-glcolor = 0x2c6bd0
-# -------------------------- cogs list
-cogss = {
-    'server',
-    'events',
-    'dev',
-    'slash_cmds',
-}
-# -------------------------- bot settings
-bot = commands.Bot(command_prefix="'",
-                intents=disnake.Intents.all(),
-                activity=disnake.Game("Collecting information!")
-                )
+import disnake
+
+GLOBAL_COLOR = 0x2c6bd0
+
+bot = commands.Bot(
+    command_prefix="'",
+    intents=disnake.Intents.all(),
+    activity=disnake.Game("Collecting information!"),
+)
 bot.remove_command("help")
-# -------------------------- set
+
 if __name__ == '__main__':
-    for cog in os.listdir("./cogs"):
-        if cog.endswith(".py"):
-            try:
-                cog = f"cogs.{cog.replace('.py', '')}"
-                bot.load_extension(cog)
+    try:
+        bot.load_extensions("./cogs")
+        print("Success! cogs loaded.")
+    except Exception as error:
+        print("Error!\n", error)
 
-            except Exception as e:
-                print(f"{cog} Can not be loaded\n")
-                print(f"{e}\n")
-
-            else:
-                print("{} has been succesfully Loaded.".format(cog))
-    # -------------------------- get token and start bot
     bot.run(os.environ.get("BOT_TOKEN"))
