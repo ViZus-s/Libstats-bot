dba16c662f28c720a9c19ce7ea49aa14e2724747
helish <52331802+helish88@users.noreply.github.com>
2022-07-26 14:40:17 +0300

    improved code, added type hinting, code formatted with [black](https://github.com/psf/black)

--- parsers/github.py
+++ parsers/github.py
@@ -1,40 +1,42 @@
-import time
-
+from dateutil import parser
+from typing import Dict
+from disnake.utils import format_dt
 from bs4 import BeautifulSoup
 import requests as req
 
-library_links = {"disnake": "https://github.com/DisnakeDev/disnake/commit/master",
-                 "nextcord": "https://github.com/nextcord/nextcord/commit/master",
-                 "pycord": "https://github.com/Pycord-Development/pycord/commit/master",
-                 "discord.py": "https://github.com/Rapptz/discord.py/commit/master",
-                 "interactions.py": "https://github.com/interactions-py/library/commit/stable",
-                }
+library_links: Dict[str, str] = {
+    "disnake": "https://github.com/DisnakeDev/disnake/commit/master",
+    "nextcord": "https://github.com/nextcord/nextcord/commit/master",
+    "pycord": "https://github.com/Pycord-Development/pycord/commit/master",
+    "discord.py": "https://github.com/Rapptz/discord.py/commit/master",
+    "interactions.py": "https://github.com/interactions-py/library/commit/stable",
+}
 
-def parsing_git(library: str):
+
+def parsing_git(library: str) -> Dict[str, str]:
 
     response = req.get(library_links[library])
-    soup = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(response.content, "html.parser")
 
-    stars = soup.find("span", {'class': "Counter js-social-count"}).text
-    forks = soup.find("span", {'class': "Counter"}).text
+    stars = soup.find("span", {"class": "Counter js-social-count"}).text
+    forks = soup.find("span", {"class": "Counter"}).text
     issues = soup.find("span", {"id": "issues-repo-tab-count"}).text
     pull_requests = soup.find("a", {"id": "pull-requests-tab"}).text
+    raw_last_commit = soup.find("relative-time")["datetime"]
+    last_commit = format_dt(parser.parse(raw_last_commit), "R")
 
-    last_commit = str(soup.find("relative-time")).split(
-        ">", maxsplit=1)[0].split("datetime=")[-1][1:-2].replace("T", " ")
-
-    last_commit = f"<t:{int(time.mktime(time.strptime(last_commit, '%Y-%m-%d %H:%M:%S')))}>"
-
-    return {"stars": stars,
-            "forks": forks,
-            "issues": issues,
-            "pull requests": pull_requests.replace("\n", "")[13:],
-            "last commit": last_commit, }
+    return {
+        "stars": stars,
+        "forks": forks,
+        "issues": issues,
+        "pull requests": pull_requests.replace("\n", "")[13:],
+        "last commit": last_commit,
+    }
 
 
 def parsing_update():
 
     response = req.get("https://github.com/ViZus-s/Libstats-bot/commit/main")
-    soup = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(response.content, "html.parser")
 
-    return soup.find('div', {"class": "commit-title markdown-title"}).text.strip()
+    return soup.find("div", {"class": "commit-title markdown-title"}).text.strip()
