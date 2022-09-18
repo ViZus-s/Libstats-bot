2f4a831781d3867a118644bd81d23a41d243f7fd
ViZus <86660567+ViZus-s@users.noreply.github.com>
2022-09-18 19:35:00 +0300

    C# support(info)

--- cogs/slash_cmds.py
+++ cogs/slash_cmds.py
@@ -18,7 +18,7 @@ class Slashes(commands.Cog):
 
         embed2 = disnake.Embed(
             title=":book: | Help",
-            description="`/stats_1` - shows any statistics[github/pypi]\n`/stats_2` - shows monthly downloads\n`/info` - shows bot info\n`/links` - shows the links of the selected library",
+            description="`/stats_1` - shows any statistics\n`/stats_2` - shows monthly downloads(python)\n`/info` - shows bot info\n`/links` - shows the links of the selected library",
             color=GLOBAL_COLOR)
         await inter.send(embed=embed2, ephemeral=True)
 
@@ -30,7 +30,7 @@ class Slashes(commands.Cog):
 
         embed = disnake.Embed(
             title="Bot information",
-            description=f"<:disnake:922937443039186975> **Disnake version**: `{disnake.__version__}`\n<:icon2:983249100558434355> **Python version**: `{version[:6:]}`\n:timer: **Ping**: `{round(self.bot.latency * 1000)} ms`\n:envelope_with_arrow: **Last update**: `{await parsing_update()}`\n:gear: **Lib support**: python libs\n:open_file_folder: **Bot github**: ||https://github.com/ViZus-s/Libstats-bot||",
+            description=f"<:disnake:922937443039186975> **Disnake version**: `{disnake.__version__}`\n<:icon2:983249100558434355> **Python version**: `{version[:6:]}`\n:timer: **Ping**: `{round(self.bot.latency * 1000)} ms`\n:envelope_with_arrow: **Last update**: `{await parsing_update()}`\n:gear: **Lib support**: Python, C# libs\n:open_file_folder: **Bot github**: ||https://github.com/ViZus-s/Libstats-bot||",
             color=GLOBAL_COLOR)
 
         embed.set_author(name=inter.author, icon_url=inter.author.display_avatar.url)
