ee4e496d96a1956ac8ceae38cea1741334e3342a
ViZus <86660567+ViZus-s@users.noreply.github.com>
2022-11-26 20:37:49 +0300

    div fix

--- parsers/python.py
+++ parsers/python.py
@@ -48,7 +48,8 @@ async def parsing_pypi(library: str) -> dict:
             reqs = json.loads(reqs)
 
     div1 = soup1.find("div", {"class": "wrapper"}).text.split()
-    div1 = [num for num in div1 if num.replace(",", "").isdigit()]
+    div1 = [num for num in div1 if num.replace(",", "").isdigit() or "." in num]
+
     last_day = f"{date.today() - timedelta(days=1)}"
 
     downloads_list = reqs["downloads"][last_day]
@@ -60,7 +61,7 @@ async def parsing_pypi(library: str) -> dict:
     try:
         if library == "disnake":
 
-            return {"last_version": div1[29], "downloads": div1, }, {
+            return {"last_version": div1[0], "downloads": div1[1:], }, {
                 "last_version": last_version2,
                 "total_downloads": f"{reqs['total_downloads']:,d}",
                 "downloads_sum": f"{sum(items_list):,d}",
@@ -70,7 +71,7 @@ async def parsing_pypi(library: str) -> dict:
 
         elif library == "nextcord":
 
-            return {"last_version": div1[33], "downloads": div1, }, {
+            return {"last_version": div1[0], "downloads": div1[1:], }, {
                 "last_version": last_version2,
                 "total_downloads": f"{reqs['total_downloads']:,d}",
                 "downloads_sum": f"{sum(items_list):,d}",
@@ -80,7 +81,7 @@ async def parsing_pypi(library: str) -> dict:
 
         elif library == "pycord":
 
-            return {"last_version": div1[29], "downloads": div1, }, {
+            return {"last_version": div1[0], "downloads": div1[1:], }, {
                 "last_version": last_version2,
                 "total_downloads": f"{reqs['total_downloads']:,d}",
                 "downloads_sum": f"{sum(items_list):,d}",
@@ -89,7 +90,7 @@ async def parsing_pypi(library: str) -> dict:
             }
         elif library == "discord.py":
 
-            return {"last_version": div1[28], "downloads": div1, }, {
+            return {"last_version": div1[0], "downloads": div1[1:], }, {
                 "last_version": last_version2,
                 "total_downloads": f"{reqs['total_downloads']:,d}",
                 "downloads_sum": f"{sum(items_list):,d}",
@@ -99,7 +100,7 @@ async def parsing_pypi(library: str) -> dict:
 
         elif library == "interactions.py":
 
-            return {"last_version": div1[33], "downloads": div1, }, {
+            return {"last_version": div1[0], "downloads": div1[1:], }, {
                 "last_version": last_version2,
                 "total_downloads": f"{reqs['total_downloads']:,d}",
                 "downloads_sum": f"{sum(items_list):,d}",
@@ -114,7 +115,7 @@ async def parsing_pypi(library: str) -> dict:
             last_version2.sort(key=lambda x: tuple((x + "z").split(".")))
             last_version2 = last_version2[-1]
 
-            return {"last_version": div1[34], "downloads": div1, }, {
+            return {"last_version": div1[0], "downloads": div1[1:], }, {
                 "last_version": last_version2,
                 "total_downloads": f"{reqs['total_downloads']:,d}",
                 "downloads_sum": f"{sum(items_list):,d}",
